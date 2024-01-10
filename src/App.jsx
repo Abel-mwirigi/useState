729c91bff11e43f7dd1b7e5bc53230bbcf8204df
Abel-mwirigi <mwirigiabel223359@daystar.ac.ke>
2024-01-10 11:19:42 +0300

    multiple useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 
 const App = ()=> {
-  const [{count, count2}, setCount] = useState({count:10, count2:20});
+  const [count, setCount] = useState(10);
+  const [count2, setCount2] = useState(20);
 
   return (
     <div>
-      <button onClick={()=>setCount(currentState => ({count:currentState.count + 1,
-      count2:currentState.count2 + 1}))}>+</button>
+      <button
+        onClick={()=>{setCount(c => c + 1);
+      setCount2(c => c + 1);}}>+</button>
       <div>count 1:{count}</div>
       <div>count 2:{count2}</div>
     </div>
